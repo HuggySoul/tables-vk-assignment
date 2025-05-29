30b3ad4818ce3af9b9fb429f9b93aa76305f2bc2
Fedchenko Artem <artem_fedchenko_2017@mail.ru>
2025-05-29 20:02:29 +0700

    feat: метод для изменения отзыва в бд

--- src/features/form/api/sendForm.api.ts
+++ src/features/form/api/sendForm.api.ts
@@ -4,17 +4,31 @@ import type { Review } from "../../../shared/types/tableRecord.type";
 /** Добавление нового отзыва */
 export const AddReview = async (review: Omit<Review, "id">) => {
   try {
-    const res = ApiRequest<Review>("/reviews", {
+    return await ApiRequest<Review>("/reviews", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(review),
     });
-
-    return res;
   } catch (error) {
     console.error("Error adding review: \n", error);
+    return null;
+  }
+};
+
+/** Обновление отзыва */
+export const UpdateReview = async (updatedReview: Review) => {
+  try {
+    return await ApiRequest<Review>(`/reviews/${updatedReview.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedReview),
+    });
+  } catch (error) {
+    console.error("Error updating review: \n", error);
     return null;
   }
 };
